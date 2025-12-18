ec4f1b2ca1c1532592daee891bda6b93c20ee691
Sean <sean.may.developer@gmail.com>
2025-12-17 22:35:37 -0500

    moved packages dir and config dir

--- setups/config/fish/functions/edit-fish.fish
+++
@@ -1,3 +0,0 @@
-function edit-fish --wraps='nvim ~/.config/fish/config.fish' --description 'alias edit-fish=nvim ~/.config/fish/config.fish'
-    nvim ~/.config/fish/config.fish $argv
-end
--- setups/config/fish/functions/show-pic.fish
+++
@@ -1,3 +0,0 @@
-function show-pic --wraps='kitten icat' --description 'alias show-pic=kitten icat'
-    kitten icat $argv
-end
